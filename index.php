b2253ef2166df8d0d2f62b1f7e6fe23e0ef406a0
Kyle Kenny Tan <kyriedrew11@gmail.com>
2022-05-05 21:59:25 +0800

    SOng1

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+SESSION_START();
+//Song Titles
+$song_lyrics1 = "Humble";
+$song_lyrics2 = "Loyal";
+$song_lyrics3 = "Tempo";
+$song_lyrics4 = "God's Plan";
+$song_lyrics5 = "Paumaga";
+//current url redirection
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+
+//condition to know if the button is clicked
+if(isset($_REQUEST['search_button'])=== true){
+    //song title url
+    if($_REQUEST['song_title'] == $song_lyrics1){
+        header("Location: ".$url_add."?humble");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics2){
+        header("Location: ".$url_add."?loyal");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics3){
+        header("Location: ".$url_add."?tempo");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics4){
+        header("Location: ".$url_add."?godsplan");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics5){
+        header("Location: ".$url_add."?paumaga");
+    }
+        //session variable
+        $_SESSION['ses_lyrics1'] = $song_lyrics1;
+        $_SESSION['ses_lyrics2'] = $song_lyrics2;
+        $_SESSION['ses_lyrics3'] = $song_lyrics3;
+        $_SESSION['ses_lyrics4'] = $song_lyrics4;
+        $_SESSION['ses_lyrics5'] = $song_lyrics5;
+          
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> K-Songs </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to K-Songs</h1>
+                <p class="lead">“This songs make me feel great when I feel lonely and alone.”</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <form method="POST" class="login-form">
+                            <div class="form-group">
+
+
+                                <?php
+                                if(isset($_REQUEST['humble']) === true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song1.php");
+                                }else if(isset($_REQUEST['loyal'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song2.php");
+                                }else if(isset($_REQUEST['tempo'])===true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song3.php");
+                                }else if(isset($_REQUEST['godsplan'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song4.php");
+                                }else if(isset($_REQUEST['paumaga'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song5.php");
+                                    
+                                ?>
+                                <div class="form-group">
+                                    <input type="text" class="form-control rounded-left" placeholder="Song Title here" name="song_title" ></div>
+                                    <button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="search_button"> Search </button>
+                                </div>
+                            </div>
+                        </form>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kyle Kenny Tan </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ song1.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> K-Songs </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to K-Songs</h1>
+                <p class="lead">“This songs make me feel great when I feel lonely and alone.”</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <form method="POST" class="login-form">
+                            <div class="form-group">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Summer Song</h2>";
+                        echo "<p class='lead '>Silent Sanctuary</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> Nobody pray for me</p>";
+                        echo "<p> It been that day for me Way </p>";
+                        echo "<p> (Yeah, yeah) </p>";
+
+
+                        echo "<p> Ayy, I remember syrup sandwiches and crime allowances</p>";
+                        echo "<p> Finesse a nigga with some counterfeits</p>";
+                        echo "<p> But now I'm countin' this</p>";
+                        echo "<p> Parmesan where my accountant lives</p>";
+                        echo "<p> In fact, I'm downin' this</p>";
+                        echo "<p> D'usse with my boo bae, tastes like Kool-Aid for the analysts</p>";
+                        echo "<p> Girl, I can buy yo' ass the world with my paystub</p>";
+                        echo "<p> Ooh, that pussy good, won't you sit it on my taste bloods?</p>";
+                        echo "<p> I get way too petty once you let me do the extras</p>";
+                        echo "<p> Pull up on your block, then break it down, we playin' Tetris</p>";
+                        echo "<p> A.m. To the P.M., P.M. To the A.M., funk</p>";
+                        echo "<p> Piss out your per diem, you just gotta hate 'em, fun</p>";
+                        echo "<p> If I quit your BM, I still ride Mercedes, funk (sige na, sige na, sige na)</p>";
+                        echo "<p> If I quit this season, I still be the greatest, funk</p>";
+                        echo "<p> My left stroke just went viral</p>";
+                        echo "<br/>";
+                        echo "<p> Right stroke put lil' baby in a spiral</p>";
+                        echo "<p> Soprano C, we like to keep it on a high note</p>";
+                        echo "<p> It's levels to it, you and I know</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> Bitch, be humble (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up lil' bitch, hol' up lil' bitch) be humble</p>";
+                        echo "<p> ((Hol' up, bitch) sit down (Sit down, lil' bitch) </p>";
+                        echo "<p> Be humble</p>";
+                        echo "<p> F(Hol' up, hol' up, hol' up, hol' up) bitch, sit down</p>";
+                        echo "<p> Lil' bitch (hol' up, lil' bitch) be humble</p>";
+                        echo "<p> (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up, hol' up, hol' up, hol' up) be humble</p>";
+                        echo "<p> Bitch, be humble (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up lil' bitch, hol' up lil' bitch) be humble</p>";
+                        echo "<p> ((Hol' up, bitch) sit down (Sit down, lil' bitch) </p>";
+                        echo "<p> Be humble</p>";
+                        echo "<p> F(Hol' up, hol' up, hol' up, hol' up) bitch, sit down</p>";
+                        echo "<p> Lil' bitch (hol' up, lil' bitch) be humble</p>";
+                        echo "<p> (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up, hol' up, hol' up, hol' up) </p>";
+
+                        echo "</br>";
+
+                        echo "<p> Who dat nigga thinkin' that he frontin' on man, man? (Man, man)</p>";
+                        echo "<p> Get the fuck off my stage, I'm the Sandman (Sandman)</p>";
+                        echo "<p> Get the fuck off my dick, that ain't right </p>";
+                        echo "<p> I make a play fuckin' up your whole life</p>";
+                        echo "<p> I'm so fuckin' sick and tired of the Photoshop</p>";
+                        echo "<p> Show me somethin' natural like afro on Richard Pryor</p>";
+                        echo "<p> Show me somethin' natural like ass with some stretchmarks</p>";
+                        echo "<p> Still'll take you down right on your mama's couch in Polo socks, ayy</p>";
+                        echo "<p> You do not amaze me, ayy</p>";
+                        echo "<p>I blew cool from AC, ayy</p>";
+                        echo "<p> Obama just paged me, ayy </p>";
+                        echo "<p> I don't fabricate it, ayy</p>";
+                        echo "<p> Most of y'all be fakin', ayy</p>";
+                        echo "<p> I stay modest 'bout it, ayy</p>";
+                        echo "<p> This that Grey Poupon, that Evian, that TED Talk, ayy</p>";
+                        echo "<p> Watch my soul speak, you let the meds talk, ayy</p>";
+                        echo "<p> If I kill a nigga, it won't be the alcohol, ayy</p>";
+                        echo "<p> I'm the realest nigga after all</p>";
+
+                        echo "<br/>";
+
+                        echo "<p> Bitch, be humble (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up lil' bitch, hol' up lil' bitch) be humble</p>";
+                        echo "<p> ((Hol' up, bitch) sit down (Sit down, lil' bitch) </p>";
+                        echo "<p> Be humble</p>";
+                        echo "<p> F(Hol' up, hol' up, hol' up, hol' up) bitch, sit down</p>";
+                        echo "<p> Lil' bitch (hol' up, lil' bitch) be humble</p>";
+                        echo "<p> (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up, hol' up, hol' up, hol' up) be humble</p>";
+                        echo "<p> Bitch, be humble (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up lil' bitch, hol' up lil' bitch) be humble</p>";
+                        echo "<p> ((Hol' up, bitch) sit down (Sit down, lil' bitch) </p>";
+                        echo "<p> Be humble</p>";
+                        echo "<p> F(Hol' up, hol' up, hol' up, hol' up) bitch, sit down</p>";
+                        echo "<p> Lil' bitch (hol' up, lil' bitch) be humble</p>";
+                        echo "<p> (Hol' up, bitch) sit down</p>";
+                        echo "<p> (Hol' up, hol' up, hol' up, hol' up) </p>";
+
+                        echo "</br>";
+                        
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kyle Kenny Tan </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
